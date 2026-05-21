dba21d92024fb99820ca8ae6692ad24f7769c02b
André Marques <a14816@oficina.pt>
2026-05-21 09:37:14 +0000

    Commitada

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import App from './App.jsx'
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+//import Header from './Header.jsx'
+//import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('roota')).render(
   <StrictMode>
-    <Header />
     <App />
-    <Footer />
   </StrictMode>,
 )
+
+
+/*
+SItes uteis
+FLATICON - Biblioteca com varios icons
+BOOTSTRAP - Biblioteca com varios "Pedacos" de codigo em HTMl, CSS
+*/
